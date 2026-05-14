66808b7402dde317a776769c5bf3153ca46de070
HeoTaehoon7 <greenclass.hoon@gmail.com>
2026-05-14 16:25:51 +0900

    Board7  PdsList 완료

--- src/main/java/com/green/pds/controller/PdsController.java
+++ src/main/java/com/green/pds/controller/PdsController.java
@@ -11,7 +11,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.green.menus.dto.MenuDTO;
 import com.green.menus.mapper.MenuMapper;
+import com.green.paging.dto.Pagination;
+import com.green.paging.dto.SearchDto;
+import com.green.pds.dto.PdsDto;
 import com.green.pds.mapper.PdsMapper;
+import com.green.pds.service.PdsService;
 
 @Controller
 @RequestMapping("/Pds")
@@ -22,6 +26,9 @@ public class PdsController {
 	
 	@Autowired
 	private   PdsMapper    pdsMapper;
+	
+	@Autowired
+	private   PdsService   pdsService;
 	
 	// /Pds/List?menu_id=MENU01&nowpage=1
 	// /Pds/List?menu_id=MENU01&nowpage=3&searchType=title&keyword=11
@@ -41,16 +48,38 @@ public class PdsController {
 		int            totalCount    =  pdsMapper.count( map );  // menus_id, searchType, keyword    
 		System.out.println("totalCount:" + totalCount);
 		
+		int         nowpage   =  Integer.parseInt( String.valueOf( map.get("nowpage") ) );  
 		
+		// 페이징을 위한 설정
+		SearchDto      searchDto     =  new SearchDto();
+		searchDto.setPageNo( nowpage );   // 현재 페이지 설정
+		searchDto.setNumOfRows( 10 );     // 한페이지에 10줄의 자료
+		searchDto.setPageSize( 10 );      // 페이지 번호 목록
+		
+		// Pagination 설정
+		Pagination    pagination  =  new Pagination(totalCount, searchDto);
+		searchDto.setPagination(pagination);	
+		
+		int           offset      =  searchDto.getOffset();
+		int           numOfRows   =  searchDto.getNumOfRows();
+		
+		map.put("offset",    offset);
+		map.put("numOfRows", numOfRows);
+		
+		System.out.println("map2:"+ map);
+		
+		// 자료 조회
+		List<PdsDto>  pdsList     =  pdsService.getPdsList( map );  
+				
 		//------------------------------------------------------
 		ModelAndView   mv        =   new ModelAndView();
 		mv.setViewName("pds/list");
 		
 		mv.addObject("menuList",   menuList);		
-		// mv.addObject("searchDto",  searchDto);		
+		mv.addObject("searchDto",  searchDto);		
+		mv.addObject("pdsList",    pdsList);			
 		
-		
-		mv.addObject("map",   map);
+		mv.addObject("map",        map);
 		return        mv;		
 		
 	}
